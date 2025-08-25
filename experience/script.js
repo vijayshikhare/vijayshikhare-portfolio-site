13e7659b1d27416877090412096eab36bab73802
vijay shikhare <shikharevijay1015@gmail.com>
2025-08-25 20:04:41 +0530

    Initial commit

---
+++ experience/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if(window.scrollY>60){
+            document.querySelector('#scroll-top').classList.add('active');
+        }else{
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange',
+function(){
+    if(document.visibilityState === "visible"){
+        document.title = "Vijay Shikhare | Portfolio Website";
+        $("#favicon").attr("href", "assets/images/vijays.jpg");
+    }
+    else {
+        document.title = "Come Back To Portfolio Website";
+        $("#favicon").attr("href", "assets/images/vijays.jpg");
+    }
+});
